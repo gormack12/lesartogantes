4264a6697d65ac44e6e256c1db10cf9f53387991
gzhugues <tchouala25@gmail.com>
2017-11-13 00:12:51 +0100

    test

--- resources/views/idea.blade.php
+++ resources/views/idea.blade.php
@@ -4,7 +4,9 @@
 	<div class="col-md-12">
 		<div class="row">
 			@if (count($idea == 1))
-
+				<div class="col-md-6 col-md-offset-3">
+					<p>{!! nl2br($idea->content) !!}</p>
+				</div>
 			@endif
 		</div>
 	</div>
