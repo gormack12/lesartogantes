7b74c5f8a8ba3dc86989ccbcddef54ba3dc7d98a
gzhugues <tchouala25@gmail.com>
2017-11-13 00:48:41 +0100

    test

--- resources/views/idea.blade.php
+++ resources/views/idea.blade.php
@@ -4,9 +4,10 @@
 	<div class="col-md-12">
 		<div class="row">
 			@if (count($idea == 1))
-			<div class="col-md-6 col-md-offset-3">
-				<p>{!! count($idea) == 1 ? nl2br($idea->content) : '' !!}</p>
-			</div>
+				<div class="col-md-6 col-md-offset-3">
+					<p>{!! count($idea) == 1 ? nl2br($idea->content) : '' !!}</p>
+				</div>
+			@endif
 		</div>
 	</div>
 @endsection
